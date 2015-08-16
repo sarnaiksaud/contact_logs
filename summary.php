c479089a258e2063ccf017da394c01e2aef6c71d
Saud Sarnaik <sarnaik.saud@gmail.com>
2015-08-16 16:28:14 +0530

    updated summary.php, error caused due to merging

--- summary.php
+++ summary.php
@@ -30,31 +30,6 @@ else
 {
 	die ("Error");
 }*/
-
-$stmt = oci_parse($conn, "select NVL(MAX(start_date),'01-JAN-2015'), NVL(MAX(end_date),SYSDATE) from dates");
-if(oci_execute($stmt))
-{
-	$name_row = oci_fetch_array($stmt);
-	$start_date = $name_row[0];
-	$end_date = $name_row[1];
-}
-else
-{
-	die ("Error");
-}
-
-/*
-$stmt = oci_parse($conn, 'select * from check_login');
-if(oci_execute($stmt))
-{
-	$name_row=oci_fetch_array($stmt);
-	if($name_row[0] != 1)
-		die ("Wrong credentials <br><a href='login.php'>Login</a>");
-}
-else
-{
-	die ("Error");
-}*/
 ?>
 <H1>
 	<?php echo "DATA SHOWN IS BETWEEN <i>$start_date</i> AND <i>$end_date</i>"; ?>
@@ -206,7 +181,6 @@ else
 	</div>
 </div>
 
-<<<<<<< .mine
 <div class="wrapper ">
 	<div class="container">
 		<div class="left">
@@ -318,119 +292,6 @@ else
 	</div>
 </div>
 
-=======
-<div class="wrapper ">
-	<div class="container">
-		<div class="left">
-			<h3>Longest call</h3>
-			<br>
-			<?php
-				$query = "select * from longest_call";
-				$stmt = oci_parse($conn, $query);
-				if(oci_execute($stmt))
-				{
-					echo "<table>";
-					echo "<tr><th>Name</th>";
-					echo "<th>Duration</th>"; 
-					// echo "<th>Count</th>";
-					echo "</tr>";
-					while($name_row=oci_fetch_array($stmt))
-					{
-						echo "<tr>";
-						echo "<td>$name_row[0]</td>";
-						echo "<td>$name_row[1]</td>";
-						// echo "<td>$name_row[3]</td>";
-						echo "</tr>";
-					}
-					echo "</table>";
-				}
-			?>
-		</div>
-	</div>
-	<div class="container">
-		<div class="left">
-			<h3>Summary call</h3>
-			<br>
-			<?php
-				$query = "select * from summary";
-				$stmt = oci_parse($conn, $query);
-				if(oci_execute($stmt))
-				{
-					echo "<table>";
-					echo "<tr><th>Name</th>";
-					echo "<th>Duration</th>"; 
-					// echo "<th>Count</th>";
-					echo "</tr>";
-					while($name_row=oci_fetch_array($stmt))
-					{
-						echo "<tr>";
-						echo "<td>$name_row[0]</td>";
-						echo "<td>$name_row[1]</td>";
-						// echo "<td>$name_row[3]</td>";
-						echo "</tr>";
-					}
-					echo "</table>";
-				}
-			?>
-		</div>
-	</div>
-	<div class="container">
-		<div class="left">
-			<h3>Top 10 Missed Caller</h3>
-			<br>
-			<?php
-				$query = "select * from top_10_M_count";
-				$stmt = oci_parse($conn, $query);
-				if(oci_execute($stmt))
-				{
-					echo "<table>";
-					echo "<tr><th>Name</th>";
-					// echo "<th>Duration</th>"; 
-					echo "<th>Count</th>";
-					echo "</tr>";
-					while($name_row=oci_fetch_array($stmt))
-					{
-						echo "<tr>";
-						echo "<td>$name_row[0]</td>";
-						// echo "<td>$name_row[1]</td>";
-						echo "<td>$name_row[3]</td>";
-						echo "</tr>";
-					}
-					echo "</table>";
-				}
-			?>
-		</div>
-	</div>
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> .theirs
 <!--
 <div class="wrapper ">
 	<div class="container">
